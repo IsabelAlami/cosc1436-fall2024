7e094bcea0f696b7b596d30689b77de01e568552
Isabel Alamillo <isabel.alamillo@my.tccd.edu>
2024-09-18 01:59:59 -0500

    finish this tomorrow girl OR ELSE

---
+++ labs/Lab1/GradeCalcualtor/GradeCalcualtor.cpp
@@ -0,0 +1,68 @@
+// Lab 1
+// Isabel Alamillo 2401402
+// COSC 1436 Fall Semester 2024
+
+#include <iostream>
+#include <string>
+#include <cmath> 
+#include <iomanip> 
+
+using namespace std;
+
+int main()
+{
+    cout << "Lab 1" << endl;
+    cout << "Isabel Alamillo 2401402" << endl;
+    cout << "COSC 1436 Fall Semester" << endl;
+
+    //Ask for name, lab grades, and exam grades
+    
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    getline(cin, name);
+
+    double labgrOne;
+    cout << "Enter a lab grade: ";
+    cin >> labgrOne;
+
+    double labgrTwo;
+    cout << "Enter a lab grade: ";
+    cin >> labgrTwo;
+
+    double labgrThree;
+    cout << "Enter a lab grade: ";
+    cin >> labgrThree;
+
+    double labgrFour;
+    cout << "Enter a lab grade: ";
+    cin >> labgrFour;
+
+    double examgrOne;
+    cout << "Enter an exam grade: ";
+    cin >> examgrOne;
+
+    double examgrTwo;
+    cout << "Enter an exam grade: ";
+    cin >> examgrTwo;
+
+    double examgrThree;
+    cout << "Enter an exam grade: ";
+    cin >> examgrThree;
+
+    //Display Username and Grades/Labs
+
+    //cout << left << setw(10) << 
+    
+    
+
+
+
+
+
+
+
+
+    
+}
+
