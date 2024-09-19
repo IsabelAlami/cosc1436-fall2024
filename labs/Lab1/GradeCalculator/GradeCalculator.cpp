b0dbdabc67910811705392c678ac444aa2664501
Isabel Alamillo <isabel.alamillo@my.tccd.edu>
2024-09-18 19:37:22 -0500

    up to s3 of lab

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -1,20 +1,62 @@
-// GradeCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab 1
+// Isabel Alamillo 2401402
+// COSC 1436 Fall Semester 2024
 
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    cout << "Lab 1" << endl;
+    cout << "Isabel Alamillo 2401402" << endl;
+    cout << "COSC 1436 Fall Semester" << endl;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    //Ask for name, lab grades, and exam grades
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    getline(cin, name);
+
+    int labgrOne;
+    cout << "Enter a lab grade: ";
+    cin >> labgrOne;
+
+    int labgrTwo;
+    cout << "Enter a lab grade: ";
+    cin >> labgrTwo;
+
+    int labgrThree;
+    cout << "Enter a lab grade: ";
+    cin >> labgrThree;
+
+    int labgrFour;
+    cout << "Enter a lab grade: ";
+    cin >> labgrFour;
+
+    int examgrOne;
+    cout << "Enter an exam grade: ";
+    cin >> examgrOne;
+
+    int examgrTwo;
+    cout << "Enter an exam grade: ";
+    cin >> examgrTwo;
+
+    int examgrThree;
+    cout << "Enter an exam grade: ";
+    cin >> examgrThree;
+
+    //Display Username and Grades/Labs
+
+    cout << name << endl;
+    cout << "Lab Grades" << endl;
+    cout << labgrOne << endl;
+    cout << labgrTwo << endl;
+    cout << labgrThree << endl;
+    cout << labgrFour << endl;
+
+    double labAvg;
