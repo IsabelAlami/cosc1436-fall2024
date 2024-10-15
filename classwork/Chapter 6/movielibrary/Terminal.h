141c5f8343ecac7bba9c9ac7f6bb49e90e837e76
Isabel Alamillo <isabel.alamillo@my.tccd.edu>
2024-10-14 21:20:31 -0500

    chapter six notes :3

---
+++ classwork/Chapter 6/movielibrary/Terminal.h
@@ -0,0 +1,6 @@
+#pragma once
+
+//Function prototypes (function forwarding)
+int ReadInt(std::string, int, int);
+std::string ReadString(std::string message);
+std::string ReadString(std::string message, bool);
