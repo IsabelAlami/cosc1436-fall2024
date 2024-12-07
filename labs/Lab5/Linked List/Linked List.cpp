cbb2237cbc2ba410387f7dc8de406aadad2b122b
Isabel Alamillo <isabel.alamillo@my.tccd.edu>
2024-12-07 06:40:45 -0600

    completed lab! yippie!!!

--- labs/Lab5/Linked List/Linked List.cpp
+++ labs/Lab5/Linked List/Linked List.cpp
@@ -17,10 +17,105 @@ struct Node
     Node* Next = nullptr;
 };
 
+/// @brief Creation of Linked List
 struct LinkedList
 {
     Node* Head = nullptr;
 };
+
+void InitializeList(LinkedList& List)
+{
+    List.Head = nullptr;
+}
+
+void AddNodeToList(LinkedList& List)
+{
+    int value;
+    cout << "Add a new Value: ";
+    cin >> value;
+
+    Node* newValue = new Node;
+    newValue->Value = value;
+    newValue->Next = nullptr;
+
+    if (!List.Head)
+    {
+        List.Head = newValue;
+    } else
+    {
+        Node* placeholder = List.Head;
+        while (placeholder->Next)
+        {
+            placeholder = placeholder->Next;
+        }
+        placeholder->Next = newValue;
+    }
+}
+
+void DeleteNodeFromList(LinkedList& List)
+{
+    int number;
+    cout << "Delete a Value: ";
+    cin >> number;
+
+    Node* previous = nullptr;
+    Node* current = List.Head;
+
+    while (current && current->Value != number)
+    {
+            previous = current;
+            current = current->Next;
+    };
+
+    if (current)
+    {
+        if (previous)
+        {
+            previous->Next = current->Next; 
+        } 
+        else
+        {
+            List.Head = current->Next;
+        }
+        delete current;
+    }
+    else
+    {
+        cout << "ERROR: Value not found in list" << endl;
+    }
+}
+
+void ClearList(LinkedList& List)
+{  
+    cout << "Clear your List?";
+    char response;
+    cout << "Are you sure you want to delete everything? Select the Character 'Y' or 'y' for Confirmation: " << endl;
+    cin >> response;
+
+    if (response == 'y' || response == 'Y')
+    {
+        Node* current = List.Head;
+
+        while (current != nullptr)
+        {
+            Node* next = current->Next;
+
+            current = next;
+        };
+        List.Head = nullptr;
+    }
+}
+
+void PrintList(const LinkedList& List)
+{
+    cout << "Printed List Values: " << endl;
+    Node* current = List.Head;
+    while (current)
+    {
+        cout << current->Value << " " << endl;
+        current = current->Next;
+    }
+}
 
 /// @brief Requests an integer value
 /// @return Integer value entered
@@ -32,14 +127,20 @@ int GetMenuCommand()
         cout << "Enter a Command: ";
         cin >> value;
 
-        if (value == 1 || value == 2 || value == 3 || value == 4)
+        if (value == 1 || value == 2 || value == 3 || value == 4 || value == 5)
             return value;
-        else if (value == 5)
-            break;
         else
             cout << "ERROR: Please enter a valid command Input" << endl;
     }
     return value;
+}
+
+/// @brief Displays Student and Lab Information
+void Display()
+{
+    cout << "Lab 5: Linked List" << endl;
+    cout << "Isabel Alamillo" << endl;
+    cout << "COSC 1436 Fall Semester 2024" << endl;
 }
 
 /// @brief Displays a menu for the user
@@ -54,14 +155,41 @@ int DisplayMenu()
     cout << "(4) Clear" << endl;
     cout << "(5) Quit" << endl;
 
-    int command = GetMenuCommand();
-
-    return command;
+    int value = GetMenuCommand();
+    
+    return value;
 }
 
 int main()
 {
-    DisplayMenu();
+    LinkedList List;
+    InitializeList(List);
+    Display();
+    while (true)
+    {
+        int command = DisplayMenu();
+
+        if (command == 1)
+        {
+            PrintList(List);
+        }
+        else if (command == 2)
+        {
+            AddNodeToList(List);
+        }
+        else if (command == 3)
+        {
+            DeleteNodeFromList(List);
+        }
+        else if (command == 4)
+        {
+            ClearList(List);
+        }
+        else if (command == 5)
+        {
+            break;
+        }
+    }
 }
 
 
